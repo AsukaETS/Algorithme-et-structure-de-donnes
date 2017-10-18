c55ae7bb02a08ae6ad1f5790959cc58540f50ad4
AsukaETS <asuka.ets@gmail.com>
2017-10-18 09:25:19 +0200

    Add files via upload
    Modification de la méthode mortalité

--- TP5/Aquarium.java
+++ TP5/Aquarium.java
@@ -19,12 +19,10 @@ public class Aquarium
    {
       lesProies = new ArrayList();
       this.lesPredateurs = new ArrayList();
-      for (int i =0; i<NbProies; i++) {
-         this.lesProies.add( new Proie( Aleatoire(0, ESPACEVITALX) , Aleatoire(0, ESPACEVITALY) )  ); }
-         for (int i =0; i<NbPredateurs; i++) {
-         this.lesPredateurs.add( new Predateur( Aleatoire(0, ESPACEVITALX) , Aleatoire(0, ESPACEVITALY) )  ); }
-   	   	
-      maFenetre = new Fenetre("Proies Predateurs", 500, 500, this.lesProies, this.lesPredateurs, 0, Color.RED, Color.BLACK); // Création de la fenêtre graphique
+      
+      this.lesProies.add( new Proie( Aleatoire(0, ESPACEVITALX) , Aleatoire(0, ESPACEVITALY) )  ); 
+      
+      maFenetre = new Fenetre("Proies Predateurs", 500, 500, this.lesProies, this.lesPredateurs, 0, Color.RED, Color.BLACK); // Crï¿½ation de la fenï¿½tre graphique
    	
    }
 
@@ -39,13 +37,10 @@ public class Aquarium
          Proie.regenererNourritureProies(5);
          System.out.println("Predateurs :"+this.lesPredateurs.size()+"  Proies :"+this.lesProies.size() );
       	
-         mortalité();
-         reproProies() ;
-         reproPredateurs() ;
          temps++;
          
          // affichage dans la vue
-         maFenetre.mise_a_jour(this.lesProies, this.lesPredateurs, temps); // mise en jour des variables modèle
+         maFenetre.mise_a_jour(this.lesProies, this.lesPredateurs, temps); // mise en jour des variables modï¿½le
          maFenetre.repaint(); // on redessine la vue
          try {
             Thread.sleep(100);   // on ralentit la simulation 0.1s ici
@@ -61,43 +56,17 @@ public class Aquarium
       {
          (this.lesProies.get(j)).deplacement();
       }
-      for( int j=0; j< this.lesPredateurs.size(); j++)
-      {
-         (this.lesPredateurs.get(j)).deplacement();
-      }
-
    	
    }
    	
-   private void mortalité()
+   private void mortalite()
    {
      		
-         // 1) vieilles proies
-      for (int i=0; i<lesProies.size(); i++) {
-         if (this.lesProies.get(i).getVie() <= 0 ){
-            this.lesProies.remove(i) ;
-         }
-      }
+         // 1) vielles proies
          
          // 2) vieux predateurs
-         for (int i=0; i<lesPredateurs.size(); i++) {
-         if (this.lesPredateurs.get(i).getVie() <= 0 ){
-            this.lesPredateurs.remove(i) ;
-         }
-      }
-
          
          // 3) predation
-         for (int i = 0; i < this.lesProies.size(); i++) {
-         for (int j = 0; j < this.lesPredateurs.size(); j++) {
-            if (this.lesProies.get(i).getX() == this.lesPredateurs.get(j).getX() && 
-               this.lesProies.get(i).getY() == this.lesPredateurs.get(j).getY() &&
-               this.lesPredateurs.get(j).getFaim() < 75) {
-               this.lesPredateurs.get(j).mange();
-               this.lesProies.get(i).setVie(0) ;
-            }
-         }
-      }
    
    		
    }
@@ -105,57 +74,13 @@ public class Aquarium
    private void reproProies()
    {
       ArrayList <Proie> bebe = new ArrayList();
-      for (int i = 0; i < this.lesProies.size(); i++) {
-         for (int j = 0; j < this.lesProies.size(); j++) {
-            if (this.lesProies.get(i).getX() == this.lesProies.get(j).getX() && 
-                this.lesProies.get(i).getY() == this.lesProies.get(j).getY() &&
-                this.lesProies.get(i).getVie()<= 250 &&
-                this.lesProies.get(j).getVie() <= 250 &&
-                i != j ) {
-               int al = Aleatoire(1,2) ;
-               if (al == 1) {
-                  bebe.add(new Proie( Aleatoire(0, ESPACEVITALX) , Aleatoire(0, ESPACEVITALY)));
-               }
-               else {
-                  bebe.add(new Proie( Aleatoire(0, ESPACEVITALX) , Aleatoire(0, ESPACEVITALY)));
-                  bebe.add(new Proie( Aleatoire(0, ESPACEVITALX) , Aleatoire(0, ESPACEVITALY)));
-               }
-            }
-         }
-      }
-      for (int x=0 ; x < bebe.size(); x++) {
-         lesProies.add(bebe.get(x)) ;
-      }
+   		
    }
-   
-   private void reproPredateurs() 
-   {
-      ArrayList <Predateur> bebe = new ArrayList();
-      for (int i = 0; i < this.lesPredateurs.size(); i++) {
-         for (int j = 0; j < this.lesPredateurs.size(); j++) {
-            if (this.lesPredateurs.get(i).getX() == this.lesPredateurs.get(j).getX() && 
-                this.lesPredateurs.get(i).getY() == this.lesPredateurs.get(j).getY() &&
-                this.lesPredateurs.get(i).getVie()<= 100 &&
-                this.lesPredateurs.get(j).getVie() <= 100 &&
-                i != j ) {
-               int al = Aleatoire(1,2) ;
-               if (al == 1) {
-                  bebe.add(new Predateur( Aleatoire(0, ESPACEVITALX) , Aleatoire(0, ESPACEVITALY)));
-               }
-               else {
-                  bebe.add(new Predateur( Aleatoire(0, ESPACEVITALX) , Aleatoire(0, ESPACEVITALY)));
-                  bebe.add(new Predateur( Aleatoire(0, ESPACEVITALX) , Aleatoire(0, ESPACEVITALY)));
-               }
-            }
-         }
-      }
-      for (int x=0 ; x < bebe.size(); x++) {
-         lesPredateurs.add(bebe.get(x)) ;
-      }
-   }
+   	
+   	
 
-   	
-   	
+
+
    private int Aleatoire( int min, int max )
    {
       return (int) ((Math.random()*(max-min+1))+ min);
