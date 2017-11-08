812b96bbf3d05f26775bf4c5e57e60c2542699c1
AsukaETS <asuka.ets@gmail.com>
2017-11-08 09:31:20 +0100

    TestExpression updated

--- TD7/Syntax.java
+++ TD7/Syntax.java
@@ -31,7 +31,7 @@ public class Syntax
          else /*if (c == '0' || c == '1' || c == '2' || 
          			c == '3' || c == '4' || c == '5' || 
          			c == '6' || c == '7' || c == '8' || 
-         			c == '9') */
+         			c == '9')*/
          {
             String s1 = ""+c;
             int v = Integer.parseInt(s1);
